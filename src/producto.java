82593241e692d78e63b0e195840559555e32e892
ramirito <ramirito@LAPTOP-LENOVO>
2025-08-31 19:50:31 -0600

    primeraclase

---
+++ src/producto.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author ramirito
+ */
+public class producto {
+    
+    
+    String codigo;
+    String nombre;
+    String categoria;
+    double precio;
+    int cantidad;
+
+    public producto(String codigo, String nombre, String categoria, double precio, int cantidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+}
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+
